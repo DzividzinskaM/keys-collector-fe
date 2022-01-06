6612a1ef9f3b3a708e07e2fe49957bc13813ed21
ouroboroser <evgeniyagorshchyk@gmail.com>
2022-01-06 15:40:31 +0200

    add chars to view responce

--- client/src/components/FindForm/FindForm.js
+++ client/src/components/FindForm/FindForm.js
@@ -1,8 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import './FindForm.scss';
 import { Link } from 'react-router-dom';
 import { languages } from './availableLanguages';
+import { test } from './test';
+import { interval } from 'rxjs';
+import { filter, take, map, Observable, from } from 'rxjs/operators';
+import Axios from  'axios-observable';
+import { data } from './dataForChart';
+import { WebSocket } from 'websocket';
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from "recharts";
+
 
 const server_url = 'https://localhost:44330/home';
 
@@ -12,6 +30,8 @@ export const FindForm = () => {
   const [pageNumbers, setPageNumbers] = useState(0);
   const [language, setLanguage] = useState('');
   const [resData, setResData] = useState('');
+  const [users, setUsers] = useState([]);
+  const ws = useRef(null);
 
   const testReq = () => {
 
@@ -21,34 +41,53 @@ export const FindForm = () => {
       alert('You have to specify correct type of language');
     };
 
+    const data = {
+      keyword,
+      pageNumbers: Number(pageNumbers),
+      language,
+    };
 
-    // const instance = axios.create({
-    //   baseURL: '',
-    //   withCredentials: false,
-    //   headers: {
-    //     'Access-Control-Allow-Origin': '*',
-    //     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    //     Authorization: token,
-    //   },
-    // });
+    const userInfo = JSON.parse(localStorage.getItem('user'));
+    const accessToken = userInfo.access_token;
 
-    // instance
-    //   .post('https://localhost:44330/home', {
-    //     keyword,
-    //     pageNumbers: parseInt(pageNumbers),
-    //     language,
-    //   })
-    //   .then((response) => {
-    //     setResData(response.data);
-    //     console.log('data', response.data);
-    //   })
-    //   .catch((error) => console.log(error));
+    console.log('work');
+
+    axios.post('http://659e-109-229-30-204.ngrok.io/home/connect', data, { headers: { 'Authorization': accessToken}})
+    .then(response => {
+      console.log(response);
+    })
+    .catch(e => {
+      console.log(e);
+    });
+
+  
+    const source = new EventSource('http://659e-109-229-30-204.ngrok.io/home', { withCredentials: false });
+
+    source.onopen = function (event) {
+      console.log('open:');
+    };
+    
+    source.onmessage = function (event) {
+      document.getElementById('msg').innerHTML += '<h2>' + event.data + '</h2>';
+    };
+
+    source.onerror = function (e) {
+      //console.log(e);
+      console.error("⛔ EventSource failed: ", e);
+      // console.log('message!');
+      // console.log('error', e);
+
+      // source.close();
+    };
+
+  //     this.setState({ ws: ws });
+
+  //     that.timeout = 250; // reset timer to 250 on open of websocket connection 
+  //     clearTimeout(connectInterval); // clear Interval on on open of websocket connection
+
   };
+  
 
-  useEffect(() => {
-    const current_user = JSON.parse(localStorage.getItem('user'));
-    setToken(current_user.access_token);
-  }, []);
 
   return (
     <div className="wrapper">
@@ -79,7 +118,32 @@ export const FindForm = () => {
             ))}
           </div>
         )}
+        <div className = 'chartResult'> 
+        <ResponsiveContainer width = '100%' height = {500}>
+        <LineChart data={data} margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+        >
+          <CartesianGrid strokeDasharray="2 20" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+          type="monotone"
+          dataKey="pv"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+          />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+          </ResponsiveContainer>
+        </div>
       </div>
+      <div id = 'msg'> </div>
     </div>
   );
 };
---
+++ client/src/components/FindForm/test.js
@@ -0,0 +1,18 @@
+export const test = [
+    {
+        title: 'test 1',
+        number: 10,
+    },
+    {
+        title: 'test 2',
+        number: 5,
+    },
+    {
+        title: 'test 3',
+        number: 3,
+    },
+    {
+        title: 'test 4',
+        number: 20,
+    },
+];
